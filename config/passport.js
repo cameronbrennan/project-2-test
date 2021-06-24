6eebac228d3d2039b197dbcaff53918d76f49af4
cameronbrennan <cameron.brennan@colorado.edu>
2021-06-24 16:47:43 -0600

    heroku6

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-const User = require('../models/User.js');
+const User = require('../models/User');
 
 
 // configuring Passport!
